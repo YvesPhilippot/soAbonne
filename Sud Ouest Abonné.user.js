88585823fd219b38315b78b20320571fd17a79ae
YvesPhilippot <yvessnake@hotmail.com>
2015-12-16 10:04:41 +0100

    Create Sud Ouest Abonné.user.js

---
+++ Sud Ouest Abonné.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Sud Ouest Abonné
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Afficher les news abonnés Sud Ouest
+// @author       El_Rhonin
+// @match        http://www.sudouest.fr/*
+// @grant        GM_addStyle
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+/* jshint -W097 */
+// Your code here...
+$(document).ready(function() {
+    
+   
+        $(".short").toggle();
+        $(".long").toggle();
+
+   
+});
